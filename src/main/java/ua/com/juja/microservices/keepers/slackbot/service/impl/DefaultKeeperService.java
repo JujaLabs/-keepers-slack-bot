8d867114443c5cb524eaea75f5a00ba182a8bf30
Ivan Shapovalov <ivshapovalov@gmail.com>
2017-12-14 13:47:04 +0300

    #64 Replace in services Slack id by Slack User

--- src/main/java/ua/com/juja/microservices/keepers/slackbot/service/impl/DefaultKeeperService.java
+++ src/main/java/ua/com/juja/microservices/keepers/slackbot/service/impl/DefaultKeeperService.java
@@ -25,18 +25,18 @@ public class DefaultKeeperService implements KeeperService {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private KeeperRepository keeperRepository;
-    private SlackIdHandlerService slackIdHandlerService;
+    private SlackUserHandlerService slackUserHandlerService;
 
     @Inject
-    public DefaultKeeperService(KeeperRepository keeperRepository, SlackIdHandlerService slackIdHandlerService) {
+    public DefaultKeeperService(KeeperRepository keeperRepository, SlackUserHandlerService slackUserHandlerService) {
         this.keeperRepository = keeperRepository;
-        this.slackIdHandlerService = slackIdHandlerService;
+        this.slackUserHandlerService = slackUserHandlerService;
     }
 
     @Override
-    public String sendKeeperAddRequest(String fromUserId, String text) {
+    public String sendKeeperAddRequest(String fromSlackUser, String text) {
         logger.debug("Started create slackParsedCommand and create keeper request");
-        SlackParsedCommand slackParsedCommand = slackIdHandlerService.createSlackParsedCommand(fromUserId, text);
+        SlackParsedCommand slackParsedCommand = slackUserHandlerService.createSlackParsedCommand(fromSlackUser, text);
         KeeperRequest keeperRequest = new KeeperRequest(slackParsedCommand.getFromUser().getUuid(),
                 receiveToUser(slackParsedCommand).getUuid(),
                 receiveToDirections(slackParsedCommand));
@@ -49,7 +49,8 @@ public class DefaultKeeperService implements KeeperService {
 
         if (ids.length > 0) {
             result = String.format("Thanks, we added a new Keeper: %s in direction: %s",
-                    slackParsedCommand.getFirstUserFromText().getSlackId(), keeperRequest.getDirection());
+                    SlackParsedCommand.wrapSlackUserInFullPattern(slackParsedCommand.getFirstUserFromText().getSlackUser()),
+                    keeperRequest.getDirection());
         } else {
             result = "ERROR. Something went wrong. Keeper was not added :(";
         }
@@ -57,9 +58,9 @@ public class DefaultKeeperService implements KeeperService {
     }
 
     @Override
-    public String sendKeeperDeactivateRequest(String fromUserId, String text) {
+    public String sendKeeperDeactivateRequest(String fromSlackUser, String text) {
         logger.debug("Started create slackParsedCommand and create keeper request");
-        SlackParsedCommand slackParsedCommand = slackIdHandlerService.createSlackParsedCommand(fromUserId, text);
+        SlackParsedCommand slackParsedCommand = slackUserHandlerService.createSlackParsedCommand(fromSlackUser, text);
         KeeperRequest keeperRequest = new KeeperRequest(slackParsedCommand.getFromUser().getUuid(),
                 receiveToUser(slackParsedCommand).getUuid(),
                 receiveToDirections(slackParsedCommand));
@@ -72,7 +73,8 @@ public class DefaultKeeperService implements KeeperService {
 
         if (ids.length > 0) {
             result = String.format("Keeper: %s in direction: %s deactivated",
-                    slackParsedCommand.getFirstUserFromText().getSlackId(), keeperRequest.getDirection());
+                    SlackParsedCommand.wrapSlackUserInFullPattern(slackParsedCommand.getFirstUserFromText().getSlackUser()),
+                    keeperRequest.getDirection());
         } else {
             result = "ERROR. Something went wrong. Keeper was not deactivated :(";
         }
@@ -80,40 +82,42 @@ public class DefaultKeeperService implements KeeperService {
     }
 
     @Override
-    public String getKeeperDirections(String fromUserId, String text) {
+    public String getKeeperDirections(String fromSlackUser, String text) {
         logger.debug("Started create slackParsedCommand and create keeper request");
-        SlackParsedCommand slackParsedCommand = slackIdHandlerService.createSlackParsedCommand(fromUserId, text);
+        SlackParsedCommand slackParsedCommand = slackUserHandlerService.createSlackParsedCommand(fromSlackUser, text);
         if (slackParsedCommand.getFromUser().equals(slackParsedCommand.getFirstUserFromText())) {
-            throw new WrongCommandFormatException("Your own slackid in command. To get your " +
+            throw new WrongCommandFormatException("Your own slack in command. To get your " +
                     "own directions use another command");
         }
         KeeperRequest keeperRequest = new KeeperRequest(slackParsedCommand.getFromUser().getUuid(),
                 slackParsedCommand.getFirstUserFromText().getUuid(),
-                slackParsedCommand.getTextWithoutSlackIds());
+                slackParsedCommand.getTextWithoutSlackUsers());
 
-        return getKeeperDirectionsFromRepository(keeperRequest, slackParsedCommand.getFirstUserFromText().getSlackId());
+        return getKeeperDirectionsFromRepository(keeperRequest, slackParsedCommand.getFirstUserFromText().getSlackUser());
     }
 
     @Override
-    public String getMyDirections(String fromUserId) {
+    public String getMyDirections(String fromSlackUser) {
         logger.debug("Started create slackParsedCommand and create keeper request");
-        SlackParsedCommand slackParsedCommand = slackIdHandlerService.createSlackParsedCommand(fromUserId, "");
+        SlackParsedCommand slackParsedCommand = slackUserHandlerService.createSlackParsedCommand(fromSlackUser, "");
         KeeperRequest keeperRequest = new KeeperRequest(slackParsedCommand.getFromUser().getUuid(),
                 slackParsedCommand.getFromUser().getUuid(),
-                slackParsedCommand.getTextWithoutSlackIds());
+                slackParsedCommand.getTextWithoutSlackUsers());
 
-        return getKeeperDirectionsFromRepository(keeperRequest, slackParsedCommand.getFirstUserFromText().getSlackId());
+        return getKeeperDirectionsFromRepository(keeperRequest, slackParsedCommand.getFirstUserFromText().getSlackUser());
     }
 
-    private String getKeeperDirectionsFromRepository(KeeperRequest keeperRequest, String keeperSlackId) {
+    private String getKeeperDirectionsFromRepository(KeeperRequest keeperRequest, String keeperSlackUser) {
         logger.debug("Received request to get directions of keeper with uuid: [{}]", keeperRequest.toString());
         String[] directions = keeperRepository.getKeeperDirections(keeperRequest);
         logger.info("Received response from keeperRepository: [{}]", Arrays.toString(directions));
 
-        String responseMessage = "The keeper " + keeperSlackId + " has no active directions.";
+        String responseMessage = "The keeper " + SlackParsedCommand.wrapSlackUserInFullPattern(keeperSlackUser) +
+                " has no active directions.";
 
         if (directions.length > 0) {
-            responseMessage = "The keeper " + keeperSlackId + " has active directions: " + Arrays.toString(directions);
+            responseMessage = "The keeper " + SlackParsedCommand.wrapSlackUserInFullPattern(keeperSlackUser) +
+                    " has active directions: " + Arrays.toString(directions);
         }
         return responseMessage;
     }
@@ -123,14 +127,15 @@ public class DefaultKeeperService implements KeeperService {
         int userCount = slackParsedCommand.getUserCountInText();
 
         if (userCount > 1) {
-            throw new WrongCommandFormatException(String.format("We found %d slack ids in your command: '%s' " +
-                            "You can not perform actions with several slack ids.",
+            throw new WrongCommandFormatException(String.format("We found %d slack users in your command '%s'. " +
+                            "You can not perform actions with several slack users.",
                     slackParsedCommand.getUserCountInText(), slackParsedCommand.getText()));
         }
 
         if (userCount == 0) {
-            throw new WrongCommandFormatException(String.format("We didn't find any slack id in your command. '%s' " +
-                    "You must write the user's slack id to perform the action with keepers.", slackParsedCommand.getText()));
+            throw new WrongCommandFormatException(String.format("We didn't find any slack user in your command '%s'. " +
+                            "You must write the user's slack to perform the action with keepers.",
+                    slackParsedCommand.getText()));
         }
 
         return slackParsedCommand.getFirstUserFromText();
@@ -138,18 +143,18 @@ public class DefaultKeeperService implements KeeperService {
 
     private String receiveToDirections(SlackParsedCommand parsedCommand) {
 
-        String textWithoutSlackIds = parsedCommand.getTextWithoutSlackIds();
+        String textWithoutSlackUsers = parsedCommand.getTextWithoutSlackUsers();
 
-        if (textWithoutSlackIds.length() == 0) {
-            throw new WrongCommandFormatException(String.format("We didn't find direction in your command: '%s' " +
+        if (textWithoutSlackUsers.length() == 0) {
+            throw new WrongCommandFormatException(String.format("We didn't find direction in your command '%s'. " +
                     "You must write the direction to perform the action with keepers.", parsedCommand.getText()));
         }
 
-        if (textWithoutSlackIds.split(" ").length > 1) {
-            throw new WrongCommandFormatException(String.format("We found several directions in your command: '%s' " +
-                    "You can perform the action with keepers on one direction only.", parsedCommand.getTextWithoutSlackIds()));
+        if (textWithoutSlackUsers.split(" ").length > 1) {
+            throw new WrongCommandFormatException(String.format("We found several directions in your command '%s'. " +
+                    "You can perform the action with keepers on one direction only.", parsedCommand.getTextWithoutSlackUsers()));
         }
 
-        return parsedCommand.getTextWithoutSlackIds();
+        return parsedCommand.getTextWithoutSlackUsers();
     }
 }
--- src/main/java/ua/com/juja/microservices/keepers/slackbot/service/impl/SlackUserHandlerService.java
+++ src/main/java/ua/com/juja/microservices/keepers/slackbot/service/impl/SlackUserHandlerService.java
@@ -2,14 +2,16 @@ package ua.com.juja.microservices.keepers.slackbot.service.impl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ua.com.juja.microservices.keepers.slackbot.model.SlackParsedCommand;
 import ua.com.juja.microservices.keepers.slackbot.model.dto.UserDTO;
 import ua.com.juja.microservices.keepers.slackbot.service.UserService;
 
 import javax.inject.Inject;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -21,47 +23,47 @@ import java.util.stream.Collectors;
  * @author Oleksii Skachkov
  */
 @Service
-public class SlackIdHandlerService {
+public class SlackUserHandlerService {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private UserService userService;
 
     @Inject
-    public SlackIdHandlerService(UserService userService) {
+    public SlackUserHandlerService(UserService userService) {
         this.userService = userService;
     }
 
-    public SlackParsedCommand createSlackParsedCommand(String fromUserId, String text) {
-        Map<String, UserDTO> usersMap = receiveUsersMap(fromUserId, text);
-        UserDTO fromUserDTO = usersMap.get(fromUserId);
+    public SlackParsedCommand createSlackParsedCommand(String fromSlackUser, String text) {
+        Map<String, UserDTO> usersMap = receiveUsersMap(fromSlackUser, text);
+        UserDTO fromUserDTO = usersMap.get(fromSlackUser);
         if (usersMap.size() > 1) {
-            usersMap.remove(fromUserId);
+            usersMap.remove(fromSlackUser);
         }
         return new SlackParsedCommand(fromUserDTO, text, new ArrayList<>(usersMap.values()));
     }
 
-        private Map<String, UserDTO> receiveUsersMap(String fromSlackId, String text) {
-        List<String> slackIds = receiveAllSlackIds(text);
-        logger.debug("added \"fromSlackId\" slack id to request: [{}]", fromSlackId);
-        slackIds.add(fromSlackId);
-        logger.debug("send slack ids: {} to user service", slackIds);
-        List<UserDTO> users = userService.findUsersBySlackIds(slackIds);
+    private Map<String, UserDTO> receiveUsersMap(String fromSlackUser, String text) {
+        List<String> slackUsers = receiveAllSlackUsers(text);
+        logger.debug("Added 'fromSlackUser' slack user to request: [{}]", fromSlackUser);
+        slackUsers.add(fromSlackUser);
+        logger.debug("Send slack users: {} to user service", slackUsers);
+        List<UserDTO> users = userService.findUsersBySlackUsers(slackUsers);
         logger.debug("Receive users: {} from user service", users);
-         Map<String, UserDTO> usersMap = users.stream()
-                    .collect(Collectors.toMap(UserDTO::getSlackId, user -> user, (e1, e2) -> e1, LinkedHashMap::new));
+        Map<String, UserDTO> usersMap = users.stream()
+                .collect(Collectors.toMap(UserDTO::getSlackUser, user -> user, (e1, e2) -> e1, LinkedHashMap::new));
         logger.debug("Convert users to map '{}'", usersMap);
         return usersMap;
     }
 
-    private List<String> receiveAllSlackIds(String text) {
+    private List<String> receiveAllSlackUsers(String text) {
         List<String> result = new ArrayList<>();
-        Pattern pattern = Pattern.compile(SlackParsedCommand.SLACK_ID_PATTERN);
+        Pattern pattern = Pattern.compile(SlackParsedCommand.SLACK_USER_PATTERN);
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
             result.add(matcher.group(1).trim());
         }
-        logger.debug("Recieved slack ids: {} from text:", result.toString(), text);
+        logger.debug("Recieved slack users: {} from text:", result.toString(), text);
         return result;
     }
 }
